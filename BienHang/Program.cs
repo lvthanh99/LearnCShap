aec86e8b6270aa3ee5de1371c951318863be3541
lvthanh99 <thanh.lv10041999@gmail.com>
2021-02-22 15:12:38 +0700

    add files

---
+++ BienHang/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace BienHang
+{
+    // Bien can co gia tri khi khoi tao.
+    // Co the dung tu khoa var de thay the kieu gia tri khi dinh nghia 1 bien.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WriteLine("Hello word!");
+            int i = 0;
+            WriteLine(i);
+
+
+            Write("Nhap vao so nguyen i: ");
+            i = int.Parse(ReadLine()); // chuyen so nguyen nhap tu ban phim tu kieu string sang int
+            WriteLine("So nguyen vua nhap la: {0}", i);
+            WriteLine($"So nguyen vua nhap la {i}");
+        }
+    }
+}
