8d2b0e10c67fe6d1cf4245a2063391ecca684bc6
lvthanh99 <thanh.lv10041999@gmail.com>
2021-02-22 14:58:33 +0700

    add file

---
+++ CuPhap/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CuPhap
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello from C#");
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
